48fbe9fb78e6fadb50e611594730fa130d78f87c
Vimmi Swami <vimmi.swami@outlook.com>
2017-09-15 19:56:47 -0700

    major changes.

--- server.js
+++ server.js
@@ -1,26 +1,83 @@
 'use strict';
 var bodyParser = require('body-parser');
-var express    = require('express');
-var handle = require('./public/util/handle.js');
+var express = require('express');
 var path = require('path');
+var ejs = require('ejs');
+var fs = require('fs');
+
 // Create the app.
 var app = express();
+app.listen(8080);
+
+//Variables for EJS
+var User= " ";
+var sayhi =" ";
+var alert= 0;
+var credential= 0;
+var valid_password="xxxx";
+var valid_user="xxxx";
 
 // Use the bodyParser() middleware for all routes.
 app.use(bodyParser());
 app.use(express.static("public"));
 
+//Controller to handle once app is run
+app.get('/',
+	function (req, res) {
+		var home_content = fs.readFileSync((path.resolve('public/home.ejs')), 'utf-8');
+		var home_compiled = ejs.compile(home_content);
+		res.send(home_compiled({sayhi: sayhi}));
+    }
+);
+
+//Controller to handle validation of Login form
 app.post('/'+'login-valid',
 	function(req, res)
 	{
 		var username = req.param('username');
 		var password = req.param('password');
+		
+		if( username === valid_user && password === valid_password){// If credentials are valid, redirect to homepage and send welcome message
+			User = username;
+			sayhi = 'Hi '+User;
+			res.render('../public/home.ejs',{sayhi:sayhi});
+        }else{ // If credentials are invalid, redirect to same login page and send flag credential
+			credential=1;
+			res.render('../public/login.ejs',{credential:credential});
+		}	
+			
+	}	
+);
 
-		var html = 'password: ' + password + ' username: ' + username;
-        res.send(html);
+//Controller to render sign-in page
+app.post('/'+'sign-in',
+	function(req, res)
+	{
+		res.render('../public/login.ejs',{credential:credential});
 	}
 );
 
+//Controller to render sign-up page
+app.post('/'+'sign-up',
+	function(req, res)
+	{
+		res.redirect('/sign_up.html');
+	}
+);
+
+//Controller to render sign-out page
+app.post('/'+'sign-out',
+	function(req, res)
+	{
+		User= " ";//resetting variables, other than valid credentials for user to relogin
+		sayhi =" ";
+		alert= 0;
+		credential= 0;
+		res.redirect('/');
+	}
+);
+
+//Controller to handle validation of sign-up page
 app.post('/'+'code-post',
 	function (req, res) {
 		var firstName = req.param("first_name");
@@ -29,30 +86,30 @@ app.post('/'+'code-post',
 		var emailAddress = req.param('email');
 		var userName = req.param("username");
 		var password = req.param("password");
-
-		console.log(emailAddress);
-		handle.sendEmail(emailAddress);
-		res.sendFile(path.resolve('public/validation.html'));
+		
+		//populate the variables for server to remember user credentials
+		User = userName ;
+		valid_password = password;
+		res.render('../public/validation.ejs',{alert:alert});
     }
 );
 
-/**
- *
- */
+//Controller to handle validation of code
 app.post('/'+ 'valid-it',
 	function (req, res){
-
 		var code = req.param("code");
+		sayhi = 'Hi '+User;
+		if(code === '1111'){// If code is valid, redirect to homepage and send welcome message
+			valid_user = User;
+			credential=0;
+			res.render('../public/home.ejs',{sayhi:sayhi});
 
-		if(code == '1111'){
-
-		}else{
-			// Code is not correct
-			// Error message 
+		}else{ // If code is invalid, redirect to same page and send flag alert
+			alert = 1;
+			console.log("inside alert"+alert);
+			res.render('../public/validation.ejs',{alert:alert});
 		}
 
 	}
 );
 
-
-app.listen(8080);
