238f0829a169e5be79f43e65f5c613d4dd98458a
Vimmi Swami <vimmi.swami@outlook.com>
2017-10-07 12:55:22 -0700

    selling-post related changes..still working on final post -posting html

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const ejs = require('ejs');
 const fs = require('fs');
 const lineReader = require('line-reader');
 const db = require('./database/database');
+var multer = require('multer');
 
 // Create the app.
 var app = express();
-app.listen(8080);
+app.listen(8081);
 
 var html_file_name ='./public/index.html';
 
@@ -25,6 +26,23 @@ var credential= 0;
 // Use the bodyParser() middleware for all routes.
 app.use(bodyParser());
 app.use(express.static("public"));
+app.use(bodyParser.json());
+var file_name=[];
+var counter=0;
+
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./public/images");
+    },
+    filename: function (req, file, callback) {
+    	file_name[counter]=file.fieldname + "_" + Date.now() + "_" + file.originalname;
+    	console.log("file names:"+file_name[counter]+" and counter:"+counter );
+    	callback(null, file_name[counter]);
+        counter++;
+    }
+});
+
+let upload = multer({ storage: Storage}).array("imgUploader", 3);
 
 //Controller to render application home page
 app.get('/' ,
@@ -138,20 +156,52 @@ app.post('/'+ 'validate-code',
 	}
 );
 
+//Controller to render selling-post page
+app.get('/'+'sell',
+	function(req, res)
+	{
+		res.redirect('/posts/selling-post.html');
+	}
+);
 /**
  * Handles selling-post.html file
  */
 
 app.post('/'+'post-it',
-	function (req, res) {
-		console.log("post-it");
+	(req, res) =>{
+		counter=0;
+        
+		if(file_name[0])
+			delete file_name[0];
+		if(file_name[1])
+			delete file_name[1]; 
+		if(file_name[2])
+			delete file_name[2]; 
+		
+		upload(req, res, function (err) {
+	        if (err) {
+	            return res.end("<h1 style=\"text-align:center\">Something went wrong!<h1>"
+	            		+"<p style=\"text-align:center\"> Please <a href=\"/posts/selling-post.html\">click here</a> to go back and try again!</p>");
+	        }else{
+	        var title = req.param("title");
+			var about = req.param("about");
+			
+			var html="<!DOCTYPE html><html><head>" +
+					"<style>body{margin:10px;background-color: ##e0e0dd;}.imgclass {display: inline-block;margin-left: auto;margin-right: auto;height: 40%;width:30%;box-shadow: 2px 2px 34px #888888;border-radius: 8px;}#divclass{text-align:center;}</style></head><body>" +
+					"<h1 style=\"color:white;border: 2px solid black;border-radius: 8px;background-color: #52809f;height:10%;text-align:left\"><a href="/">Post It</a></h1>" +
+					"<h2 style=\"color:white;border: 2px solid black;border-radius: 8px;background-color: #52809f;height:10%;text-align:center\">Post uploaded sucessfully with below details!</h1>"
+				+"<h3 style=\"color:white;border: 2px solid black;border-radius: 8px;background-color: #52809f;height:10%\"> Product name: </h3><p style=\"border: 2px solid black;border-radius: 8px;background-color: white;font-style:italic\">"+title+ "</p><br/>"
+				+"<h3 style=\"color:white;border: 2px solid black;border-radius: 8px;background-color: #52809f;\"> Product description:</h3><pre style=\"border: 2px solid black;border-radius: 8px;background-color: white;font-style:italic\"> "+about+ "</pre><br/>"
+				+"<h3 style=\"color:white;border: 2px solid black;border-radius: 8px;background-color: #52809f;\"> Product picture(s):</h3>";
+			
+			html+="<div class=\"divclass\"><img class=\"imgclass\"  src='/images/"+file_name[0]+"'>"
+				+"<img class=\"imgclass\" onerror=\"this.style.display = 'none'\" src='/images/"+file_name[1]+"'>"
+				+"<img class=\"imgclass\"  onerror=\"this.style.display = 'none'\" src='/images/"+file_name[2]+"'></div></body></html>";
+						
+	        return res.end(html);
+	        }
 
-		var title = req.param("title");
-		var about = req.param("about");
-
-		console.log("title: " + title);
-
-		//db.insertToDB(object, "post");
+	    });
 
     }
 );
