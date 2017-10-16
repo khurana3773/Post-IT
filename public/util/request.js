03b98ade746eef7cb05d923dd45d6e9dd6ad2363
huyvo <huyvanvo94@gmail.com>
2017-10-16 16:37:49 -0700

    set up util files

---
+++ public/util/request.js
@@ -0,0 +1,32 @@
+/**
+ * Enable request from server
+ * Mainly for database request
+ *
+ * This page uses jQuery for request
+ */
+
+/**
+ * Request auto complete data from server
+ * The response must be an array
+ */
+function requestAutoComplete(selector){
+
+}
+
+/**
+ * Request a product 
+ * The return must be a JSON
+ * A product:
+ *  {
+ *      img (the image)
+ *      userId (Who posted this)
+ *      tag (what kind of category)
+ *      timestamp (when was this posted)
+ *      keywords (the keywords that users added)
+ *      location (the location users want to sell at)
+ *  }
+ *
+ */
+function requestProduct(selector) {
+
+}
