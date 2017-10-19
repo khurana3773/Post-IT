be202468a7419d40d828763ba1b6aa0d5c8e1b19
huyvo <huyvanvo94@gmail.com>
2017-10-18 23:28:47 -0700

    remote database access

---
+++ settings.js
@@ -0,0 +1,6 @@
+/** This is to connect to remote db **/
+
+const USER = "masterroot";
+const PASSWORD = "masterroot";
+
+const remoteURL = "mongodb://"+USER+":"+PASSWORD+"@ds125335.mlab.com:25335/post_it";
