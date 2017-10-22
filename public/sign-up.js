478645bb2b8e3addd89446d3686f1eb50e20d88b
huyvo <huyvanvo94@gmail.com>
2017-10-21 21:56:28 -0700

    update regular expression

--- public/sign-up.js
+++ public/sign-up.js
@@ -1,29 +1,35 @@
-// To check the input data against the expected length and pattern before sending to server.
 function validateForm() {
-	var fname = document.getElementById("first_name").value;
-	if (fname.length > 20) {
-		alert("First name must be max 20 characters long.");
-		return false;
+    var fname = document.getElementById("first_name").value;
+    if (fname.match(/^([^0-9])$/)) {
+        alert("Please enter a name");
+        return false;
+    }
+
+    var lname = document.getElementById("last_name").value;
+    if (lname.match(/^([^0-9])$/)) {
+        alert("Please enter a name.");
+        return false;
+    }
+
+    var studentid = document.getElementById("student_id").value;
+
+    if(!studentid.match(/^\d+$/)){
+    	alert("Student id cannot contain letters");
+    	return false;
 	}
 
-	var lname = document.getElementById("last_name").value;
-	if (lname.length > 20 ) {
-		alert("Last name must be max 20 characters long.");
-		return false;
-	}
+    if (sjsuid.length !== 9) {
+        alert("SJSUID must be 9 characters long.");
+        return false;
+    }
 
-	var sjsuid = document.getElementById("student_id").value;
-	if (sjsuid.length !== 9) {
-		alert("SJSUID must be 9 characters long.");
-		return false;
-	}
+    var email= document.getElementById("mail").value;
 
-	var email= document.getElementById("mail").value;
-	var emailRE=/^.+\.+.+@.+\..{2,4}$/;
-	if (!email.match(emailRE)) {
-		alert("Email should match the pattern: something.something@something.something");
-		return false;
-	}
+    if (!email.match(/^.+@.+\..{2,4}$/)) {
+        alert("Email should match the pattern: name@domain.com");
+        return false;
+    }
 
+    return true;
 
 }
