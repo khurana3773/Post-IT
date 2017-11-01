af98464e240227db0ba8c20796271faa1324564c
Huy Vo <huyvanvo94@gmail.com>
2017-11-01 11:33:13 -0700

    Update to jquery

--- public/sign-up.js
+++ public/sign-up.js
@@ -1,24 +1,24 @@
 function validateForm() {
-    var fname = document.getElementById("first_name").value;
+    var fname = $("#first_name").val();
     if (fname.match(/^[0-9]*$/)){
         alert("First name cannot contain numbers.");
         return false;
     }
 
-    var lname = document.getElementById("last_name").value;
+    var lname = $("#last_name").val();
     if (lname.match(/^[0-9]*$/)) {
         alert("Last name cannot contain numbers.");
         return false;
     }
 
-    var studentid = document.getElementById("student_id").value;
+    var studentid = $("#student_id").val();
 
     if(!studentid.match(/^\d+$/)){
     	alert("Student id cannot contain letters");
     	return false;
 	}
 
-    var email= document.getElementById("mail").value;
+    var email = $("#mail").val();
 
     if (!email.match(/^.+@.+\..{2,4}$/)) {
         alert("Email should match the pattern: name@domain.com");
