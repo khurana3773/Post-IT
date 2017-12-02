853a102aaa7c0e002d180e741d8c0d1cb6e0fc4d
Vimmi Swami <vimmi.swami@outlook.com>
2017-12-02 14:12:47 -0800

    search with case insensitive regex and price slidebar changes , removing console.log from index

--- routes/category.js
+++ routes/category.js
@@ -11,11 +11,7 @@ router.get('/', function (req, res) {
 	var collections = db.get("posts");
 	var input=req.param("type").split(",");
 
-	console.log("req"+ input[0]+", "+ input[1].split(":")[1]);
-	//var query = "price >= "+input[1].split(":")[1]+" AND type='"+input[0]+"'";,price:{$gte:input[1].split(":")[1]}
 	collections.find({type: input[0],price:{$gte:parseInt(input[1].split(":")[1])}}, {sort: {timestamp: 1}}, function (e, docs) {
-   // collections.find(query, {sort: {timestamp: 1}}, function (e, docs) {
-            console.log(docs);
             res.send(JSON.stringify(docs));
         });
 });
