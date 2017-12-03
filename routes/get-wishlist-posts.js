c8fcf33f85198a614bc38798e23378ceb001ed49
surajkhurana <khurana3773@gmail.com>
2017-12-02 19:31:37 -0800

    wishlist functioniality
    fronted + backend

---
+++ routes/get-wishlist-posts.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/**
+ * Return a list of post from _id
+ */
+
+router.get('/', function (req, res) {
+	var db = req.db;
+	var collections = db.get("wishlist");
+
+	console.log(req);
+	var userId = req.param("userId");
+
+	//adding title for simplification of calls
+	//todo : get postIds and then get title from the posts.
+	collections.find({userId: userId}, {"postId":1,"title":1}, function (e, docs) {
+
+		res.send(JSON.stringify(docs));
+    });
+});
+
+module.exports = router;
